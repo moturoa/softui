132da9e5e4f38294b69941d3284533e75186b4ff
Remko Duursma <remkoduursma@gmail.com>
2022-01-31 19:51:44 +0100

    update test

--- test/test_dropdown_select.R
+++ test/test_dropdown_select.R
@@ -10,17 +10,30 @@ devtools::load_all()
 
 ui <- fluidPage(theme = bs_theme(version = 5),
 
-                
-                picker_select_ui("test", "Select mtcar", width = 300),
-                
-                tags$hr(),
-                verbatimTextOutput("txt_out")
+                fluidRow(
+                  column(4, 
+                         picker_select_ui("test", "Select mtcar", width = 300)       
+                  ), 
+                  column(4,
+                         
+                    actionButton("btn_update", "Set choices")       
+                  )
+                ),
+                fluidRow(
+                  tags$hr(),
+                  verbatimTextOutput("txt_out")  
+                )
                 
 )
 
 server <- function(input, output, session) {
   
-  out <- callModule(picker_select_module, "test", rownames(mtcars))
+  out <- callModule(picker_select_module, "test", choices = rownames(mtcars), update = test_update)
+  test_update <- reactiveVal()
+  
+  observeEvent(input$btn_update, {
+    test_update(list(choices = letters, selected = letters[1:3]))
+  })
   
   
   output$txt_out <- renderPrint({
