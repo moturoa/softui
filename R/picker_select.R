38f63ce9fc3ded7be70963a955909b4ebe8e9aa7
Remko Duursma <remkoduursma@gmail.com>
2022-01-31 19:47:22 +0100

    picker_select

---
+++ R/picker_select.R
@@ -0,0 +1,110 @@
+#' Picker-select
+#' @description Replacement for `shinyWidgets::pickerInput`, which is somehow not compatible with this (bootstrap 5.1 / softui) framework.
+#' @export
+#' @rdname picker_select
+picker_select_ui <- function(id, label = "", btn_class = "btn-info", height = 300, width = 300, button_style = "", dropdown_style = ""){
+  
+  ns <- NS(id)
+  w_css <- shiny::validateCssUnit(width)
+  h_css <- shiny::validateCssUnit(width)
+  
+  drop_style <- paste(glue::glue("padding: 18px; overflow-y: auto; height: {h_css}; width: {w_css};"), dropdown_style)
+  
+  tags$div(class = "dropdown",
+           
+           actionButton(ns("btn_drop"), label, 
+                        class = paste(btn_class, "dropdown-toggle"),
+                        `data-bs-toggle`="dropdown", 
+                        `data-bs-auto-close` = "outside", 
+                        style = button_style),
+           
+           tags$div(class = "dropdown-menu", 
+                    tags$div(style = drop_style,
+                             
+                             tags$div(style = "display:inline-block;",
+                                      textInput(ns("txt_search"), NULL, width = "90%")
+                             ),
+                             actionButton(ns("btn_reset_search"), label = icon("remove"), 
+                                          class = "btn-light", style = "background: none; border: none;"),
+                             tags$br(),
+                             
+                             actionButton(ns("btn_all_on"), "Alles aan",icon = bsicon("check"), class = "btn-light btn-sm"),
+                             actionButton(ns("btn_all_off"), "Alles uit", icon = bsicon("x"), class = "btn-light btn-sm"),
+                             tags$hr(),
+                             checkboxGroupInput(ns("chk1"), NULL, choices = NULL)
+                    )
+           )
+           
+           
+           
+  )
+  
+}
+
+
+#' @rdname picker_select
+#' @export
+picker_select_module <- function(input, output, session, choices = NULL, update = reactive(NULL), 
+                                 max_choices = 50, debounce = 500){
+  
+  # must have names (here we search)
+  if(is.null(names(choices))){
+    names(choices) <- choices
+  }
+  
+  selection <- reactive({
+    input$chk1
+  })
+  
+  observeEvent(input$btn_all_on, {
+    updateCheckboxGroupInput(session, "chk1", selected = choices)
+  })
+  
+  observeEvent(update(), {
+    
+    u <- update()
+    
+    updateCheckboxGroupInput(session, "chk1", selected = u$selected, choices = u$choices)
+    
+  })
+  
+  
+  observeEvent(input$btn_reset_search, {
+    updateTextInput(session, "txt_search", value = "")  
+  })
+  
+  
+  observeEvent(input$btn_all_off, {
+    updateCheckboxGroupInput(session, "chk1", selected = character(0))
+  })
+  
+  txt_searched_instant <- reactive({
+    input$txt_search
+  })
+  
+  txt_searched <- debounce(txt_searched_instant, millis = debounce, domain = session)
+  
+  observeEvent(txt_searched(), {
+    
+    val <- txt_searched()
+    
+    if(nchar(val) < 2){
+      vals <- choices
+    } else {
+      nms_sel <- grep(val, names(choices), ignore.case = TRUE, value = TRUE)
+      vals <- choices[nms_sel]
+    }
+    
+    # --> ui warning oid, label boven choices
+    if(length(vals) > max_choices){
+      vals <- vals[1:max_choices]
+    }
+    
+    updateCheckboxGroupInput(session, "chk1", choices = vals)  
+    
+  })
+  
+  
+  return(selection)
+}
+
--- test/test_dropdown_select.R
+++ test/test_dropdown_select.R
@@ -5,87 +5,13 @@ library(shiny)
 library(bslib)
 library(shinyWidgets)
 
-
-
-picker_select_ui <- function(id, label = "", btn_class = "btn-info", height = 300){
-  
-  ns <- NS(id)
-  
-  tags$div(class = "dropdown",
-           
-           actionButton(ns("btn_drop"), label, 
-                        class = paste(btn_class, "dropdown-toggle"),
-                        `data-bs-toggle`="dropdown", 
-                        `data-bs-auto-close` = "outside"),
-           
-           tags$div(class = "dropdown-menu", 
-                    tags$div(style = glue::glue("padding: 18px; overflow-y: scroll;height: {height}px;"),
-                             
-                             searchInput(ns("txt_search"), NULL, width = "100%",
-                                         btnReset = icon("remove")),
-                             actionButton(ns("btn_all_on"), "Alles aan", class = "btn-light btn-sm"),
-                             actionButton(ns("btn_all_off"), "Alles uit", class = "btn-light btn-sm"),
-                             tags$hr(style = "padding: 2px !important;"),
-                             
-                             checkboxGroupInput(ns("chk1"), NULL, choices = NULL)
-                    )
-           )
-           
-           
-           
-  )
-  
-}
-
-
-picker_select_module <- function(input, output, session, choices = NULL, max_choices = 50){
-  
-  # must have names (here we search)
-  if(is.null(names(choices))){
-    names(choices) <- choices
-  }
-  
-  selection <- reactive({
-    input$chk1
-  })
-  
-  observeEvent(input$btn_all_on, {
-    updateCheckboxGroupInput(session, "chk1", selected = choices)
-  })
-  
-  observeEvent(input$btn_all_off, {
-    updateCheckboxGroupInput(session, "chk1", selected = character(0))
-  })
-  
-  observeEvent(input$txt_search, {
-    
-    val <- input$txt_search
-    
-    if(nchar(val) < 2){
-      vals <- choices
-    } else {
-      nms_sel <- grep(val, names(choices), ignore.case = TRUE, value = TRUE)
-      vals <- choices[nms_sel]
-    }
-    
-    # --> ui warning oid, label boven choices
-    if(length(vals) > max_choices){
-      vals <- vals[1:max_choices]
-    }
-    
-    updateCheckboxGroupInput(session, "chk1", choices = vals)  
-    
-  })
-  
-  
-return(selection)
-}
+devtools::load_all()
 
 
 ui <- fluidPage(theme = bs_theme(version = 5),
 
                 
-                picker_select_ui("test", "Select mtcar"),
+                picker_select_ui("test", "Select mtcar", width = 300),
                 
                 tags$hr(),
                 verbatimTextOutput("txt_out")
@@ -94,7 +20,7 @@ ui <- fluidPage(theme = bs_theme(version = 5),
 
 server <- function(input, output, session) {
   
-  out <- callModule(picker_select_module, "test", choices = c("Aap" = "a", "Banaan" = "b", "Citroen" = "c")) #ownames(mtcars))
+  out <- callModule(picker_select_module, "test", rownames(mtcars))
   
   
   output$txt_out <- renderPrint({
