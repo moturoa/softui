422db2e9d0534f37babde79f74fa98b5ac151495
Remko Duursma <remkoduursma@gmail.com>
2022-02-04 14:46:31 +0100

    geen unload nodig

--- R/zzz.R
+++ R/zzz.R
@@ -1,13 +1,13 @@
-.onLoad <- function(libname, pkgname) {
-  shiny::addResourcePath(
-    prefix = "shintoui_assets",
-    directoryPath = system.file(
-      "assets",
-      package = "shintoui"
-    )
-  )
-}
-
-.onUnload <- function(libname, pkgname) {
-  shiny::removeResourcePath("assets")
-}
+# .onLoad <- function(libname, pkgname) {
+#   shiny::addResourcePath(
+#     prefix = "shintoui_assets",
+#     directoryPath = system.file(
+#       "assets",
+#       package = "shintoui"
+#     )
+#   )
+# }
+# 
+# .onUnload <- function(libname, pkgname) {
+#   shiny::removeResourcePath("assets")
+# }
