00f2e4982c17088fc5649e081b6a8d3653238ce2
Remko Duursma <remko@shintolabs.nl>
2022-02-25 09:37:36 +0100

    palette

---
+++ R/shinto_palette.R
@@ -0,0 +1,16 @@
+#' Color palette(s) for Shinto apps
+#' @export
+shinto_pal <- function(){
+
+  c(
+    main = "#1C1C40",
+    accent1 = "#345483",
+    accent2 = "#84B3E0",
+    accent3 = "#E3C1B4",
+    accent4 = "#EB6247",
+    accent5 = "#346E6E",
+    accent6 = "#96D6D2"
+  )
+  
+    
+}
