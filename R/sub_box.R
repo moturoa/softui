e288f8804355f0b04ed9735fbe6b6863ba20a9ed
Remko Duursma <remko@shintolabs.nl>
2022-04-11 13:41:23 +0200

    sub_box

---
+++ R/sub_box.R
@@ -0,0 +1,26 @@
+
+#' Box within a box
+#' @description You are not allowed to place a `softui::box` inside another `softui::box`, but you can
+#' use this `softui::sub_box`
+#' @param title A title for the box
+#' @param grey_level Value between 0.1 (standard) and 0.9 (very dark), in steps of 0.1.
+#' @export
+sub_box <- function(title = NULL, grey_level = 0.1, ...){
+  
+  if((10 *grey_level) %% 1 > 0){
+    stop("provide grey_level in steps of 0.1")
+  }
+  stopifnot(grey_level >= 0.1 & grey_level <= 0.9)
+  
+  tags$ul(class = "list-group",
+    tags$li(class = glue::glue("list-group-item border-0 d-flex p-4 mb-2 bg-gray-{1000*grey_level} border-radius-lg"),
+      tags$div(class="d-flex flex-column",
+          if(!is.null(title)){
+            tags$h6(class="mb-3 text-sm", title)
+          }, ...
+      )
+    )
+  )
+  
+}
+
