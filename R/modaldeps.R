693e56d54ff9394b4de3983da00d86570ffa35c0
Remko Duursma <remko@shintolabs.nl>
2022-06-23 13:04:28 +0200

    fullscreen modal

--- R/modaldeps.R
+++ R/modaldeps.R
@@ -9,6 +9,7 @@
 #' @param remove_modal_on_confirm If TRUE, always removes the modal on OK click. 
 #' Otherwise, call `removeModal` yourself. This may be needed when some input 
 #' validation is done before the modal can be removed.
+#' @param size As in `?modalDialog` but also has a 'fullscreen' option.
 #' @param session Shiny session object (no need to set)
 #' @export
 modal <- function(..., 
@@ -22,13 +23,16 @@ modal <- function(...,
                   close_txt = "Sluiten", 
                   close_icon = softui::bsicon("x-lg"),
                   remove_modal_on_confirm = TRUE,
+                  size = c("m", "s", "l", "xl", "fullscreen"), 
+                  #fullscreen_up_to = c("always","sm","md","lg","xl","xxl"),
                   session = shiny::getDefaultReactiveDomain()){
   
   # see zzz.R; inst/logo is available under logo/
   img <- "shintologo/logoshintolabs.png"
   
-  shiny::modalDialog(
+  modal_dialog(
     easyClose = easyClose,
+    size = size, # fullscreen_up_to = fullscreen_up_to,  # lijkt geen effect te hebben
     title = shiny::tags$div(title, 
                     shiny::tags$span(
                       shiny::tags$img(src=img, width = 70), 
@@ -61,3 +65,39 @@ modal <- function(...,
   )  
   
 }
+
+# copied from shiny::modalDialog
+modal_dialog <- function (..., title = NULL, footer = modalButton("Dismiss"), 
+                         size = c("m", "s", "l", "xl", "fullscreen"), 
+                         fullscreen_up_to = c("always","sm","md","lg","xl","xxl"),
+                         easyClose = FALSE, fade = TRUE){
+  
+  size <- match.arg(size)
+  fullscreen_up_to <- match.arg(fullscreen_up_to)
+  
+  backdrop <- if (!easyClose)"static"
+  keyboard <- if (!easyClose)"false"
+  
+  class <- switch(size, s = "modal-sm", m = NULL, l = "modal-lg", xl = "modal-xl", fullscreen = "modal-fullscreen")
+  if(size  == "fullscreen" & fullscreen_up_to != "always"){
+    class <- paste0(class,"-",fullscreen_up_to,"-down")
+  }
+  
+  div(id = "shiny-modal", class = "modal", class = if (fade) 
+    "fade", tabindex = "-1", `data-backdrop` = backdrop, 
+    `data-bs-backdrop` = backdrop, `data-keyboard` = keyboard, 
+    `data-bs-keyboard` = keyboard, 
+    div(class = "modal-dialog", 
+        class = class, 
+        div(class = "modal-content", 
+            if (!is.null(title)) div(class = "modal-header", tags$h4(class = "modal-title", title)), 
+            div(class = "modal-body", ...), 
+            if (!is.null(footer)) div(class = "modal-footer", footer))), 
+    tags$script(HTML("if (window.bootstrap && !window.bootstrap.Modal.VERSION.match(/^4\\./)) {\n         var modal = new bootstrap.Modal(document.getElementById('shiny-modal'));\n         modal.show();\n      } else {\n         $('#shiny-modal').modal().focus();\n      }")))
+}
+
+
+
+
+
+
