d06199d634fcdaacbf79405f115d26cd4bdce609
Remko Duursma <remko@shintolabs.nl>
2022-07-14 14:21:47 +0200

    favicon

--- R/dashboard_page.R
+++ R/dashboard_page.R
@@ -57,6 +57,7 @@ dashboard_page <- function(header, sidebar, body, title = "",
 
   head_tag <- shiny::tags$head(
     shiny::tags$meta(charset = "utf-8"),
+    shiny::tags$link(rel="shortcut icon", href="favicon.ico"),
     shiny::tags$meta(
       name = "viewport",
       content = "width=device-width, initial-scale=1, shrink-to-fit=no"
