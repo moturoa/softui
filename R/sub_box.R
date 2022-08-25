a3fced080cb7becdaf4c03a756ee3dedeb8bc961
Remko Duursma <remko@shintolabs.nl>
2022-08-25 11:10:04 +0200

    sub_box gains margin_bottom argument

--- R/sub_box.R
+++ R/sub_box.R
@@ -4,6 +4,10 @@
 #' use this `sub_box`, or `?alert_box` for a message-type box.
 #' @param title A title for the box
 #' @param grey_level Value between 0.1 (standard) and 0.9 (very dark), in steps of 0.1.
+#' @param icon An icon (optional), placed left of the title
+#' @param collapsible If TRUE (default), can be minimized
+#' @param collapsed If TRUE, starts minimized
+#' @param margin_bottom Margin below the `sub_box`, in pixels
 #' @param \dots Any HTML (or text) items to appear in the box.
 #' @export
 sub_box <- function(..., 
@@ -11,7 +15,8 @@ sub_box <- function(...,
                     icon = NULL,
                     grey_level = 0.1,
                     collapsible = TRUE,
-                    collapsed = FALSE){
+                    collapsed = FALSE,
+                    margin_bottom = 0){
   
   grey_level <- as.numeric(grey_level)
   if((10 *grey_level) %% 1 > 0){
@@ -50,7 +55,7 @@ sub_box <- function(...,
   
   
   tags$div(class = glue::glue("border-0 p-4 {bg_css} border-radius-lg"),
-                 
+           style = glue::glue("margin-bottom: {margin_bottom}px"),
         if(!is.null(title)){
           shiny::tagList(
             shiny::tags$div(style = "width: 100% !important; height: 32px !important;",
