f9953976cd45fc788bfed71d0865c36524deea98
Remko Duursma <remko@shintolabs.nl>
2022-12-19 15:23:03 +0100

    clean

--- R/update_sidebar.R
+++
@@ -1,7 +0,0 @@
-#' Go to other 'page' (main menu item)
-#' @details Replaces 'shinydashboard::updateTabItems', and works from within modules also.
-#' @param selected The page to go to, corresponds to `tabName` in `menu_item` in the main UI
-#' @export
-go_to_page <- function(selected, session = shiny::getDefaultReactiveDomain()){
-  session$sendCustomMessage("selectMenuItem", list(selected = selected))
-}
