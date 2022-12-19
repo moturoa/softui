96b1e30ddac339530f6fa52d6d4741407a0dab7e
Remko Duursma <remko@shintolabs.nl>
2022-12-19 15:22:21 +0100

    doc

--- R/page_link.R
+++ R/page_link.R
@@ -1,4 +1,12 @@
 #' Utilities for links to pages
+#' @details 
+#' - `page_link` is used to make an anchor tag with a javascript onclick method that sets an id (+nonce) to an 
+#' input id of choice.
+#' - `route_page_link` has to be called for the input `id_out` in `page_link` to allow changing pages when
+#' a link of that id has been clicked . It calls `go_to_page` for you.
+#' - `go_to_page` replaces 'shinydashboard::updateTabItems' (also works inside modules) and is used 
+#' to change pages (a `tabName` in the softui dashboard menu).
+#' - `clicked_link_id` is a helper function 
 #' @param id Value of the ID that will be sent as link information (e.g. person ID, location ID)
 #' @param id_out Name of the input ID to set when clicked on a link (namespacing in modules is done automatically)
 #' @param label Label to appear in the link
@@ -52,6 +60,7 @@ route_page_link <- function(id,
 }
 
 #' @export
+#' @rdname page_link
 clicked_link_id <- function(id, input, session = shiny::getDefaultReactiveDomain()){
   
   shiny::reactive({
@@ -65,6 +74,14 @@ clicked_link_id <- function(id, input, session = shiny::getDefaultReactiveDomain
   })
   
   
+}
+
+
+#' @param selected The page to go to, corresponds to `tabName` in `menu_item` in the main UI
+#' @export
+#' @rdname page_link
+go_to_page <- function(selected, session = shiny::getDefaultReactiveDomain()){
+  session$sendCustomMessage("selectMenuItem", list(selected = selected))
 }
 
 
