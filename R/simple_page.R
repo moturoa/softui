e07cb5b79eabe6a61455566ea1eeb178213313a7
Remko Duursma <remko@shintolabs.nl>
2022-12-23 10:24:42 +0100

    padding simple page

--- R/simple_page.R
+++ R/simple_page.R
@@ -3,10 +3,11 @@
 #' Useful for testing or simple applications without a sidebar menu.
 #' @export
 #' @importFrom shinyjs inlineCSS
-simple_page <- function(...){
+simple_page <- function(..., padding = "40px"){
 
   add_softui_dependencies(
     shiny::tags$body(class = "bg-gray-100",
+                     style = htmltools::css(padding  = padding),
                      softui_dependencies(),
                      package_ui_dependencies(busybar_color = "#346E6E", 
                                              disconnect_message = "De sessie is verlopen of er ging iets fout!"),
