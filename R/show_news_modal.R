9078c5914950a4b7729e7158b79afe8e1873a09d
Remko Duursma <remko@shintolabs.nl>
2023-01-20 15:29:38 +0100

    news module, version read etc.

---
+++ R/show_news_modal.R
@@ -0,0 +1,121 @@
+
+
+
+#' @importFrom dplyr filter
+application_read_news <- function(category = NULL, min_version = NULL, news_file = "NEWS.md"){
+  
+  if(!file.exists(news_file)){
+    stop("Make a NEWS.md file first (see WBM, Apollo)")
+  }
+  
+  data <- tools:::.build_news_db_from_package_NEWS_md(news_file)
+  
+  if(!is.null(category)){
+    data <- dplyr::filter(data, Category =="" | grepl(category, Category))
+  }
+  
+  if(!is.null(min_version)){
+    data <- dplyr::filter(data, Version > !!min_version)
+  }
+  
+  data
+}
+
+#' @importFrom reactable reactable
+render_news_table <- function(data){
+  data |> 
+    dplyr::select(Versie = Version, Nieuws = HTML) |> 
+    reactable::reactable(columns = list(
+      Nieuws = colDef(html = TRUE),
+      Versie = colDef(maxWidth = 100)
+    ), searchable = TRUE, defaultPageSize = 5)
+  
+}
+
+#' Read software version from VERSION
+#' @export
+get_application_version = function(){
+  
+  fn <- "VERSION"
+  if(!file.exists(fn)){
+    ""
+  } else {
+    trimws(readLines(fn)[1])
+  }
+  
+}
+
+#' Shiny module to show all software news for a tenant
+#' @export
+#' @rdname tenantNewsModule
+tenantNewsModuleUI <- function(id){
+  
+  ns <- NS(id)
+  
+  tags$div(
+    class = "dropdown",
+    actionLink(ns("lnk_show_all_news"), 
+               class = "nav-link", bsicon("stars"))
+  )
+  
+}
+
+#' @export
+#' @rdname tenantNewsModule
+tenantNewsModule <- function(input, output, session, category = NULL){
+  
+  observeEvent(input$lnk_show_all_news, {
+    show_version_news_modal(changes_since = "all", category = NULL)
+  })
+  
+}
+
+#' Show modal with NEWS
+#' @param previous_version Show news since this software version (leave blank if changes_since = 'all')
+#' @param category Name of category to filter by (e.g. gemeente in WBM)
+#' @param changes_since Either 'last' (show news since previous_version) or 'all' to show all news
+#' @param header_label List with components 'all' and 'previous' with UI to place above the news table
+#' @export
+show_version_news_modal <- function(previous_version = NULL, 
+                                    category = NULL,
+                                    changes_since = c("last","all"),
+                                    session = shiny::getDefaultReactiveDomain(),
+                                    
+                                    header_label = list(
+                                      all = "Alle wijzigingen in deze applicatie.",
+                                      previous = tagList(
+                                        tags$p("Er zijn wijzigingen sinds je laatste login!"),
+                                        tags$p(HTML("Alle nieuwsberichten lezen? Klik op <i class='bi-stars'></i> in de balk rechtsboven."))
+                                      )
+                                    )
+                                    
+                                    ){
+  
+  
+  changes_since <- match.arg(changes_since)
+  
+  data <- if(changes_since == "all"){
+    application_read_news(category = category)  
+  } else {
+    application_read_news(category = category, 
+                          min_version = previous_version)
+  }
+  
+  header <- if(changes_since == "all"){
+    header_label$all
+  } else {
+    header_label$previous
+  }
+  
+  showModal(
+    softui::modal(
+      title = tagList(bsicon("stars"), "Nieuws"), 
+      size = "l", close_button = FALSE, easyClose = FALSE,
+      
+      header,
+      
+      render_news_table(data)
+    )
+  )
+  
+}
