80e3724e4f82f0c9d5d9401297286b8984c8e26a
Remko Duursma <remko@shintolabs.nl>
2023-03-03 10:57:09 +0100

    less duplicate code

--- R/tabset_panel.R
+++ R/tabset_panel.R
@@ -1,89 +1,31 @@
 
 #' Tab panel 
+#' @description Just like tab_box, but without a box-shadow. You can use a tabset_panel in a box,
+#' but you cannot use a tab_box in a box (double box shadows!).
+#' Identical to `tab_box(..., class = 'card-plain')`
+#'@param id Optional id for the tab_box
+#'@param selected Which tab is selected initially? Defaults to the first tab
+#'@param width Width of tab_box in bootstrap units (default = 12)
+#'@param fill If TRUE, makes the tab panel titles fill the entire available width
+#'@param style Extra CSS for the `card` class.
+#'@param ... Furher arguments to [tab_box()], and `tab_panel`'s
 #'@export
 tabset_panel <- function(..., 
                     id = NULL,
                     selected = NULL, 
                     width = 12,
                     fill = FALSE,
-                    type = c("pills","tabs"),
+                    type = NULL,
                     style = ""){
   
   
-  type <- match.arg(type)
-  
-  items <- dropNulls(list(...))
-  n_items <- length(items)
-  idnr <- as.character(sample(1000:9999,1))
-  
-  for(i in seq_along(items)){
-    items[[i]] <- items[[i]] %>% tagAppendAttributes(index = i)
+  if(!is.null(type)){
+    message("type argument to tabset_panel is now ignored (always pills)")
   }
   
-  atr <- lapply(items, "[[", "attribs")
-  
-  
-  if(is.null(selected)){
-    selected <- atr[[1]]$`data-value`
-  }
-  
-  lis <- lapply(atr, function(el){
-    
-    if(is.null(el$class) || el$class != "tab-pane"){
-      return(NULL)
-    }
-    
-    cl <- if(el$`data-value` == selected){
-      "nav-link active"
-    } else {
-      "nav-link"
-    }
-    
-    shiny::tags$li(class = "nav-item",
-                   shiny::tags$a(class = cl, href = glue::glue("#tab-{idnr}-{el$index}"),
-                                 `data-toggle`="tab", 
-                                 `data-bs-toggle`="tab", 
-                                 `data-value`=el$`data-value`,
-                                 el$tab_title
-                   )
-    )
-  })
-  
-  cont <- lapply(items, function(el){
-    
-    a <- el$attribs
-    shiny::tags$div(class = "tab-pane", 
-                    `data-value`=a$`data-value`, 
-                    id = glue::glue("tab-{idnr}-{a$index}"),
-                    el$children
-    )
-    
-  })
-  
-  # Add 'active' class to selected tab panel
-  vals <- sapply(cont, function(x)x$attrib$`data-value`)
-  i_act <- which(vals == selected)
-  cont[[i_act]] <- cont[[i_act]] %>% tagAppendAttributes(class = "active")
-  
-  
-  nav_cl <- glue::glue("nav nav-{type} card-header-tabs")
-  if(fill)nav_cl <- paste(nav_cl, "nav-fill")
-  
-  
-  shiny::column(width = width, id = id, 
-      shiny::tags$div(class = "card-header",
-                      shiny::tags$ul(class = nav_cl,
-                                     `data-tabsetid`= glue::glue("{idnr}"),
-                                     lis
-                      )
-      ),
-      shiny::tags$div(class = "card-body",
-                      shiny::tags$div(class = "tab-content", `data-tabsetid` = idnr,
-                                      cont
-                      )
-                      
-      )
-      
+  tab_box(
+    id = id, selected = selected, width = width, fill = fill, style = style,
+    class = "card-plain", ...
   )
   
   
