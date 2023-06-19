58e3da4f61cdd6be4a1a73414cd9119b08c2debb
Remko Duursma <remko@shintolabs.nl>
2023-06-19 16:46:04 +0200

    action_button has 'disabled' argument

--- R/action_button.R
+++ R/action_button.R
@@ -2,10 +2,12 @@
 #' @param \dots Arguments sent to `shiny::actionButton`
 #' @param class Extra CSS classes
 #' @param status Status color (see `?valid_statuses`)
+#' @importFrom shinyjs disabled
 #' @export
 action_button <- function(..., 
                           class = "",
-                          status = NULL){
+                          status = NULL,
+                          disabled = FALSE){
   
   if(!is.null(status)){
     validate_status(status)
@@ -13,6 +15,11 @@ action_button <- function(...,
   }
   
   
-  shiny::actionButton(..., class = class)
+  btn <- shiny::actionButton(..., class = class)
   
+  if(disabled){
+    btn <- shinyjs::disabled(btn)
+  }
+  
+  btn
 }
