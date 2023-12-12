404dd85b2cfe27e7216ed52485343f1e8d0dd807
WesleyBrants <wesley@shintolabs.nl>
2023-12-12 10:18:20 +0100

    Update property table adding control for NA-values

--- R/property_table.R
+++ R/property_table.R
@@ -2,6 +2,7 @@
 #' Formatted table of properties
 #' @param data A dataframe with one row. 
 #' @param full_width If TRUE, fills the container 100%
+#' @param show_na if TRUE, NA values are shown. If FALSE, rows with a value NA are not shown
 #' @param bold_property Whether the property column is in bold font (default)
 #' @param css_property CSS for the 'property' column in the table
 #' @param css_value CSS for the 'value' column in the table
@@ -16,15 +17,20 @@
 #' # Use tibble() instead of data.frame() so the names do not get changed:
 #' property_table(tibble::tibble(Kenmerk = "groot huis", Postcode = "123AB", `Aantal inwoners` = 2),
 #' full_width = FALSE)
-property_table <- function(data, full_width = TRUE,
+property_table <- function(data, full_width = TRUE, show_na = TRUE,
                            bold_property = TRUE,
                            css_property = "font-size: 1.1em;",
                            css_value = "font-size: 1.0em;"
-                           ){
+){
   
   stopifnot(nrow(data) == 1)
+  if(show_na){
+    res <- t(data) %>% as.data.frame()
+  } else {
+    res <- t(data) %>% as.data.frame() %>% setNames("value") %>% dplyr::filter(!is.na(value), !is.null(value), value != "null")
+  }
   
-  t(data) %>% as.data.frame() %>%
+  res %>%
     kableExtra::kbl(col.names = NULL) %>% 
     kableExtra::kable_styling("hover", full_width = full_width) %>%
     kableExtra::column_spec(1, bold = bold_property, extra_css = css_property) %>%
